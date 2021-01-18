0d44240620b145f797959611f03d6327823dae43
BillyG83 <bgilmore83@hotmail.co.uk>
2021-01-18 18:34:35 +0100

    feat(weather-forecast): added weekly average

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,6 +4,7 @@ import './city-selector/city-selector.js';
 import cityData from './cityData.js';
 import defaultStyles from './css/styles.js'
 import mainStyles from './css/mainStyles.js'
+import colorVars from './css/color-vars.js'
 import 'fa-icons';
 
 export class WeatherForecast extends LitElement {
@@ -56,11 +57,38 @@ export class WeatherForecast extends LitElement {
     let today = date.getDay();
     const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
+    let averageWeeklyTemp = 0;
     this.weatherData.daily.forEach((daily, index) => {
       daily.day = index === 0 ? 'Today' : weekday[today];
       today > 5 ? today = 0 : today++;
+      averageWeeklyTemp += daily.temp.day
     });
     
+    this.weeklyAverage = (averageWeeklyTemp / this.weatherData.daily.length).toFixed(0);
+    
+    switch(true) {
+      case this.weeklyAverage <= -20:
+        this.weeklyAverageColor = colorVars.none;
+        break;
+      case this.weeklyAverage > -20 && this.weeklyAverage <= -1 :
+        this.weeklyAverageColor = colorVars.blueLight;
+        break;
+      case this.weeklyAverage > -1 && this.weeklyAverage <= 10 :
+        this.weeklyAverageColor = colorVars.blue;
+        break;
+      case this.weeklyAverage > 10 && this.weeklyAverage <= 25 :
+        this.weeklyAverageColor = colorVars.green;
+        break;
+      case this.weeklyAverage > 25 && this.weeklyAverage <= 45 :
+        this.weeklyAverageColor = colorVars.red;
+        break;
+      case this.weeklyAverage > 45 :
+        this.weeklyAverageColor = colorVars.brown;
+        break;
+      default:
+        this.weeklyAverageColor = colorVars.none;
+    }
+
     this.requestUpdate();
   }
 
@@ -73,7 +101,19 @@ export class WeatherForecast extends LitElement {
   renderWeather() {
     return html`
       <section class="weather">
-        <h2>7 day forecast for ${this.cityLocation.name}</h2>
+        <div class="weather__title">
+          <h2>${this.cityLocation.name} weekly average: ${this.weeklyAverage}&deg;C</h2>
+
+          <div class="weather__thermometer">
+            <span class="color" style="background-color:${this.weeklyAverageColor}; height:${Number(this.weeklyAverage) + 50}%"></span>
+            <span class="min">-50</span>
+            <span class="mid">0</span>
+            <span class="max">50</span>
+            <span class="circle" style="background-color:${this.weeklyAverageColor};"></span>
+          </div>
+        </div>
+          </div>
+        </div>
         <div class="weather__week">
           ${this.weatherData.daily.map(
             (day, index) => html`<weather-day .dayData=${day} .index=${index}></weather-day>`
--- src/css/mainStyles.js
+++ src/css/mainStyles.js
@@ -38,12 +38,57 @@ export const mainStyles = css`
     border: none;
     text-align: center; 
   }
-  .weather h2 {
+  .weather__title {
+    align-items: center;
     background-color: ${unsafeCSS(colorVars.white)};
     border: none;
+    display: flex;
+    justify-content: center;
     margin-bottom: 12px;
     padding: 16px 8px;
     text-align: center;
+  }
+  .weather__thermometer {
+    background-color: ${unsafeCSS(colorVars.grayLight)};
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    height: 50px;
+    justify-content: flex-end;
+    margin: 0 16px;
+    position: relative;
+    width: 16px;
+  }
+  .weather__thermometer .color {
+    display: block;
+    width: 100%;
+  }
+  .weather__thermometer .min,
+  .weather__thermometer .mid,
+  .weather__thermometer .max {
+    font-size: 8px;
+    left: calc(100% + 5px);
+    line-height: 8px;
+    pointer-events: none;
+    position: absolute;
+    top: 0;
+  }
+  .weather__thermometer .max {
+    top: 0;
+  }
+  .weather__thermometer .mid {
+    top: calc(50% - 4px);
+  }
+  .weather__thermometer .min {
+    top: calc(100% - 8px);
+  }
+  .weather__thermometer .circle {
+    border-radius: 50%;
+    bottom: -5px;
+    height: 20px;
+    left: calc(50% - 10px);
+    position: absolute;
+    width: 20px;
   }
   .weather__week {
     align-items: center;
